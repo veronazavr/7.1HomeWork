1886d9c1524cf4a0de0ee5f37e329ca1d8bdaa92
Veronazavr <verochka10@list.ru>
2020-09-23 22:44:50 +0300

    added updated COVEREDRATIO

--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -8,9 +8,18 @@ class StatisticServiceTest {
     @Test
     void findMax() {
         StatisticService service = new StatisticService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
+    }
+    @Test
+    void findMax2() {
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {3, 10, 9, 2, 11, 4, 8, 1, 7, 5, 6};
+        long expected = 11;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+
     }
 }
